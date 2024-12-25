c64e0fdccd951bb0f200ec1f07be94e460a52f17
garold664 <garold664@gmail.com>
2024-12-25 09:41:28 +0300

    display LineChart

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import LineChart from './components/LineChart';
 import useData from './hooks/useData';
 
 function App() {
   const [data, isLoading, error] = useData();
 
-  const content = isLoading
-    ? 'Loading...'
-    : error
-    ? 'Error'
-    : data.map((d) => <div key={d.date}>{d.date}</div>);
+  const [categories, setCategories] = useState([]);
+  const [values, setValues] = useState([]);
+  const [dates, setDates] = useState([]);
+
+  useEffect(() => {
+    setCategories(data.map((d) => d.category));
+    setValues(data.map((d) => d.value));
+    setDates(data.map((d) => d.date));
+  }, [data]);
+
+  const transformedData = {
+    labels: dates,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: values,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 3,
+      },
+    ],
+  };
+
+  const content = isLoading ? (
+    'Loading...'
+  ) : error ? (
+    'Error'
+  ) : (
+    <LineChart data={transformedData} />
+  );
   return <div className="App">{content}</div>;
 }
 
---
+++ src/components/LineChart.jsx
@@ -0,0 +1,45 @@
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+} from 'chart.js';
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Линейный график',
+    },
+  },
+};
+
+export default function LineChart({ data }) {
+  return (
+    <div>
+      <h2>Chart</h2>
+
+      <Line data={data} options={options} />
+    </div>
+  );
+}
